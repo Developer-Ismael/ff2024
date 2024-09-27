95bf20ae7dd62b21540cd40867553d95c197426e
ismael.pvlsilva <ismael.pvlsilva@senacsp.edu.br>
2024-09-27 20:12:45 -0300

    feat: Ex1 Corrigido

---
+++ exercicio/ex1/js/script.js
@@ -0,0 +1,11 @@
+//Declare uma constante texto, uma variavel de numero inteiro, uma contante de um numero decimal e imprima no console todos juntos separados por virgula. 
+
+const texto = "Texto Corrido";
+let numeroInteiro = 10;
+const numeroDecimal = 10.5;
+
+//forma 1
+console.log(texto + " , " + numeroInteiro + " , " +numeroDecimal);
+//Forma 2
+console.log(texto + ' , ' + numeroInteiro + ' , ' +numeroDecimal);
+
