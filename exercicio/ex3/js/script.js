541ad168fa305b89d62147a76178622cd3d73fa5
ismael.pvlsilva <ismael.pvlsilva@senacsp.edu.br>
2024-09-27 21:09:07 -0300

    feat: Ex3 Corrigido

--- exercicio/ex3/js/script.js
+++ exercicio/ex3/js/script.js
@@ -15,3 +15,23 @@ let numeroDecimal3 = 5.15;
 let mensagem = (texto1 + " " + texto2 + " " + texto3 + " " + texto4+ " " + texto5 + " " + numeroInteiro1 + " " + numeroInteiro2 + " " + numeroDecimal1 + " " + numeroDecimal2 + " " + numeroDecimal3);
 
 console.log(mensagem);
+
+
+
+//correção professro
+const constante1 = 10;
+const constante2 = "Texto 1";
+const constante3 = true;
+const constante4 = 5.75;
+const constante5 = "Texto 2";
+
+let variavel1 = 10;
+let variavel2 = "Texto 3";
+let variavel3 = true;
+let variavel4 = 5.75;
+let variavel5 = "Texto 6";
+
+let msg = `${contante1} ${contante2} ${contante3} ${contante4} ${contante5}`;
+msg+= `${variavel1} ${variavel2} ${variavel3} ${variavel4} ${variavel5}`;
+ 
+console.log(msg);
