7dfff66c7d8612cee412b19cc77c929aa6030dee
ismael.pvlsilva <ismael.pvlsilva@senacsp.edu.br>
2024-09-27 21:01:05 -0300

    feat: Adicionando README.md

--- exercicio/ex3/js/script.js
+++ exercicio/ex3/js/script.js
@@ -1,1 +1,17 @@
-//Declare 5 constantes e 5 variaveis de numero inteiro, 
+//Declare 5 constantes e 5 variaveis de numero inteiro, numero decimal, texto e imprima no console todos separados por espaço.
+
+const texto1 = "Texto corrido1";
+const texto2 = "Texto corrido2";
+const texto3 = "Texto corrido3";
+const texto4 = "Texto corrido4";
+const texto5 = "Texto corrido5";
+
+let numeroInteiro1 = 1;
+let numeroInteiro2 = 2;
+let numeroDecimal1 = 3.5;
+let numeroDecimal2 = 4.10;
+let numeroDecimal3 = 5.15;
+
+let mensagem = (texto1 + " " + texto2 + " " + texto3 + " " + texto4+ " " + texto5 + " " + numeroInteiro1 + " " + numeroInteiro2 + " " + numeroDecimal1 + " " + numeroDecimal2 + " " + numeroDecimal3);
+
+console.log(mensagem);
